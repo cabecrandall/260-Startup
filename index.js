c74b7d909a5df52b66f405c8aeac574a22b0df97
cabecrandall <cabecrandall@gmail.com>
2023-11-09 16:27:00 -0700

    Got login working!

--- index.js
+++ index.js
@@ -1,12 +1,118 @@
 const express = require('express');
 const app = express();
 
-app.use(express.json());
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb'}));
 app.use(express.static('public'));
+
+class user {
+    constructor(username, password, profilePicture) {
+        this.username = username;
+        this.password = password;
+        this.profilePicture = profilePicture;
+        this.favorites = [];
+        this.history = [];
+    }
+}
+
+//Endpoints
+app.post('/user', (req, res) => {
+    const user = req.body;
+    users.push(user);
+    res.send(user);
+    console.log(users);
+});
+
+app.get('/user/:username', (req, res) => {
+    const user = getUser(req.params.username);
+    if (user) {
+        res.send(JSON.stringify(user));
+    } else {
+        res.status(404).send("User not found");
+    }
+});
+
+app.get('/user/:username/favorites', (req, res) => {
+    const user = getUser(req.body.username);
+    if (user) {
+        res.send(user.favorites);
+    } else {
+        res.status(404).send("User not found");
+    }
+});
+
+app.get('/user/:username/history', (req, res) => {
+    const user = getUser(req.params.username);
+    if (user) {
+        res.send(user.history);
+    } else {
+        res.status(404).send("User not found");
+    }
+});
+
+app.put('/user/:username', (req, res) => {
+    const user = getUser(req.params.username);
+    console.log("user: " + user);
+    if (user) {
+        user.profilePicture = req.body.profilePicture;
+        res.send(user.profilePicture);
+        updateUser(user);
+    } else {
+        res.status(404).send("User not found");
+    }
+});
+
+app.put('/user/:username/favorites', (req, res) => {
+    const user = getUser(req.params.username);
+    if (user) {
+        user.favorites.push(req.body);
+        res.send(user.favorites);
+        updateUser(user);
+    } else {
+        res.status(404).send("User not found");
+    }
+});
+
+app.put('/user/:username/history', (req, res) => {
+    const user = getUser(req.params.username);
+    if (user) {
+        user.history.push(req.body);
+        res.send(user.history);
+        updateUser(user);
+    } else {
+        res.status(404).send("User not found");
+    }
+});
+
+
+
+
+
+// Make login wooorrrrkkk!!!!
+
+
+const users = [];
+
+function getUser(username) {
+    for (let i = 0; i < users.length; i++) {
+        if (users[i].username == username) {
+            return users[i];
+        }
+    }
+    return null;
+}
+
+function updateUser(user) {
+    for (let i = 0; i < users.length; i++) {
+        if (users[i].username == user.username) {
+            users[i] = user;
+        }
+    }
+}
 
 app.listen(8080);
 
 console.log("Server running on port 8080");
 
 
-
+//Get reviews so they stay forever!
--- public/login.js
+++ public/login.js
@@ -1,48 +1,110 @@
-class user {
-    constructor(username, password, profilePicture) {
-        this.username = username;
-        this.password = password;
-        this.profilePicture = profilePicture;
-        this.favorites = [];
-        this.history = [];
-    }
-}
 
-const users = []
 
 // Function to handle form submission
-function handleLogin(event) {
+async function handleLogin(event) {
     event.preventDefault();
-
-    // check to see if user exists
-
-
     
+
     // Get the values from the form
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
 
+    //get user from storage
+    try {
+    const user = await fetch('/user/' + username);
+    const userJson = await user.json();
+    const correctPassword = userJson.password;
+
+    if (password !== correctPassword) {
+        throw new Error("Incorrect password");
+    }
+
     // Store the login information in localStorage
     localStorage.setItem("username", username);
     localStorage.setItem("password", password);
+    localStorage.setItem("profilePicture", userJson.profilePicture);
+    localStorage.setItem("favorites", JSON.stringify(userJson.favorites));
+    localStorage.setItem("history", JSON.stringify(userJson.history));
+
+    location.href = "profile.html"
+
+    // You can redirect to another page or perform other actions here
+    console.log("Login successful!");
+  }
+  catch (error) {
+    showIncorrectLogin();
+  }
+
+
+    
+}
+
+async function handleRegister(event) {
+    event.preventDefault();
+
+    // Get the values from the form
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    const file = document.getElementById("profilePicture").files[0];
+    console.log(file + "  uhhhh")
+    if (file) {
+      // Read the selected file and set it as the profile picture
+      const reader = new FileReader();
+      reader.onload = async function (e) {
+        const profilePicture = e.target.result;
+        // Store the profile picture in localStorage
+        localStorage.setItem('profilePicture', profilePicture);
+        // Send the profile picture to the server
+        const user = {username: username, password: password, profilePicture: profilePicture}
+        
+        const response = await fetch('/user', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(user)
+    });
+        // Redirect back to the profile page
+        //window.location.href = 'profile.html';
+      };
+      reader.readAsDataURL(file);
+
+    //send user to server
+    
+
+    // Store the login information in localStorage
+    localStorage.setItem("username", username);
+    localStorage.setItem("password", password);
+    localStorage.setItem("favorites", JSON.stringify([]));
+    localStorage.setItem("history", JSON.stringify([]));
 
     location.href = "profile.html"
 
     // You can redirect to another page or perform other actions here
     console.log("Login successful!");
 }
+}
 
-function setProfilePicture(event) {
+async function setProfilePicture(event) {
     event.preventDefault();
     const file = document.getElementById("profilePicture").files[0];
     console.log(file + "  uhhhh")
     if (file) {
       // Read the selected file and set it as the profile picture
       const reader = new FileReader();
-      reader.onload = function (e) {
+      reader.onload = async function (e) {
         const profilePicture = e.target.result;
         // Store the profile picture in localStorage
         localStorage.setItem('profilePicture', profilePicture);
+        // Send the profile picture to the server
+        const response = await fetch('/user/' + localStorage.getItem('username'), {
+          method: 'PUT',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({ profilePicture: profilePicture })
+        });
         // Redirect back to the profile page
         //window.location.href = 'profile.html';
       };
@@ -58,7 +120,21 @@ try {
 }
 catch (error) {
     const signupForm = document.getElementById("signupForm");
-    signupForm.addEventListener("submit", handleLogin);
+    signupForm.addEventListener("submit", handleRegister);
     signupForm.addEventListener("submit", setProfilePicture);
     console.log("found signup form")
 }
+
+
+function showIncorrectLogin() {
+  const errorMessage = document.getElementById("errorMessage");
+  if (!errorMessage) {
+    const errorMessage = document.createElement("p");
+    errorMessage.id = "errorMessage";
+    errorMessage.innerText = "Incorrect username or password";
+    errorMessage.style.color = "red";
+    const loginForm = document.getElementById("loginForm");
+    loginForm.appendChild(errorMessage);
+  }
+}
+
